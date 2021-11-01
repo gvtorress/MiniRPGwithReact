d3613460978d8dbfec49f0ea0708c93668278fab
Gabriel Torres <gvtorress@gmail.com>
2021-11-01 17:27:48 -0300

    Mini RPG

---
+++ src/components/Character/index.tsx
@@ -0,0 +1,30 @@
+import { CharacterSides } from "../../types/CharacterSides"
+import { Container, NameBox } from "./styles"
+
+type Props = {
+    x: number,
+    y: number,
+    side: CharacterSides,
+    name: string,
+}
+
+export const Character = ({x, y, side, name}: Props) => {
+    const size = 30
+    const sides = {
+        down: 0,
+        left: -30,
+        right: -60,
+        up: -90,
+    }
+
+    return (
+        <Container
+            size={size}
+            left={x * size}
+            top={y * size}
+            side={sides[side] ?? 0}
+        >
+            <NameBox>{name}</NameBox>
+        </Container>
+    )
+}
---
+++ src/hooks/useCharacter.ts
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+import { CharacterSides } from '../types/CharacterSides'
+
+import { mapSpots } from '../data/mapSpots'
+
+export const useCharacter = (nameProp: string) => {
+    const [name, setName] = useState(nameProp)
+    const [position, setPosition] = useState({ x:3, y:5 })
+    const [side, setSide] = useState<CharacterSides>('down')
+
+    const moveLeft = () => {
+        setPosition(position => ({
+            x: canMove(position.x - 1, position.y) ? position.x -1 : position.x,
+            y: position.y,
+        }))
+        setSide('left')
+    }
+
+    const moveRight = () => {
+        setPosition(position => ({
+            x: canMove(position.x + 1, position.y) ? position.x + 1 : position.x,
+            y: position.y,
+        }))
+        setSide('right')
+    }
+
+    const moveDown = () => {
+        setPosition(position => ({
+            x: position.x,
+            y: canMove(position.x, position.y + 1) ? position.y + 1: position.y,
+        }))
+        setSide('down')
+    }
+
+    const moveUp = () => {
+        setPosition(position => ({
+            x: position.x,
+            y: canMove(position.x, position.y - 1) ? position.y - 1 : position.y,
+        }))
+        setSide('up')
+    }
+
+    const canMove = (x: number, y: number) => {
+
+       if (mapSpots[y] !== undefined && mapSpots[y][x] !== undefined) {
+           return (mapSpots[y][x] === 1)
+       }
+
+        return false
+    }
+
+    return {
+        name,
+        x: position.x,
+        y: position.y,
+        side,
+        moveLeft,
+        moveRight,
+        moveDown,
+        moveUp,
+    }
+}
